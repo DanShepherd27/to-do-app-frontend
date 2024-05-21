295ca5fead26fe6b8db27c1ef4c87d82a2481e9f
DanShepherd27 <jdani20001027@gmail.com>
2024-05-22 01:16:12 +0200

    feat(ui): user creation form validation added

--- src/app/components/UserCreation/UserCreation.tsx
+++ src/app/components/UserCreation/UserCreation.tsx
@@ -1,19 +1,63 @@
 "use client";
 import React, { useState } from "react";
 import * as S from "./UserCreationAtoms";
+import { Formik, FormikHelpers } from "formik";
+import * as Yup from "yup";
+
+interface FormValues {
+  username: string;
+}
+
+const inititalValues: FormValues = { username: "" };
+
+const validationSchema = Yup.object({
+  username: Yup.string()
+    .required("Name is required")
+    .min(2, "Name too short")
+    .max(35, "Name too long")
+    .matches(/^[A-Za-z,.'-]*$/, "Special characters are not allowed"),
+});
 
 export const UserCreation = () => {
-  const handleSubmit = () => {
-    console.log("Submitted.");
+  const handleSubmit = (
+    values: FormValues,
+    { resetForm }: FormikHelpers<FormValues>
+  ) => {
+    console.log("Submitted." + values);
+    resetForm();
   };
 
   return (
     <S.UserCreationWrapper>
-      <S.UserCreationContainer>
-        <S.TitleText>Name</S.TitleText>
-        <S.UsernameInput placeholder="Your name" />
-        <S.SubmitButton onClick={handleSubmit}>Submit</S.SubmitButton>
-      </S.UserCreationContainer>
+      <Formik
+        initialValues={inititalValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched }) => (
+          <S.Form>
+            <S.Group>
+              <S.Label htmlFor="username">Name</S.Label>
+              <S.Field
+                id="username"
+                name="username"
+                placeholder="Your name"
+                autocomplete="off"
+              />
+              <S.ErrorMessage name="username" component="div" />
+            </S.Group>
+            <S.SubmitButton
+              type="submit"
+              disabled={
+                !touched.username ||
+                (!!errors.username && errors.username.length > 0)
+              }
+            >
+              Submit
+            </S.SubmitButton>
+          </S.Form>
+        )}
+      </Formik>
     </S.UserCreationWrapper>
   );
 };
--- src/app/components/UserCreation/UserCreationAtoms.ts
+++ src/app/components/UserCreation/UserCreationAtoms.ts
@@ -1,5 +1,6 @@
 "use client";
 import styled from "styled-components";
+import * as F from "formik";
 
 export const UserCreationWrapper = styled.div`
   width: 100%;
@@ -8,22 +9,30 @@ export const UserCreationWrapper = styled.div`
   align-items: center;
 `;
 
-export const UserCreationContainer = styled.div`
+export const Form = styled(F.Form)`
   width: 100%;
   height: 250px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
   border-radius: 7px;
 `;
 
-export const TitleText = styled.div`
+export const Group = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Label = styled.label`
   width: 500px;
   max-width: 80%;
   margin-bottom: 20px;
 `;
 
-export const UsernameInput = styled.input`
+export const Field = styled(F.Field)`
   width: 500px;
   max-width: 80%;
   height: 50px;
@@ -31,32 +40,45 @@ export const UsernameInput = styled.input`
   background-color: white;
   color: rgb(var(--primary-rgb));
   padding: 0 10px;
-  margin-bottom: 80px;
 
   &:focus {
     outline: 3px solid rgb(var(--primary-rgb));
   }
 `;
 
-export const SubmitButton = styled.button`
+export const ErrorMessage = styled(F.ErrorMessage)`
+  margin-top: 8px;
+  font-size: calc(var(--default-font-size) - 8px);
+  color: rgb(var(--error-rgb));
+`;
+
+export const SubmitButton = styled.button<{ $disabled?: boolean }>`
   width: 150px;
   max-width: 80%;
   height: 50px;
-  border: 3px solid rgb(var(--primary-rgb));
+  border: ${({ disabled }) =>
+    disabled
+      ? "3px solid rgb(var(--active-secondary-rgb))"
+      : "3px solid rgb(var(--primary-rgb))"};
   border-radius: 7px;
-  background-color: transparent;
-  color: rgb(var(--primary-rgb));
+  background-color: ${({ disabled }) =>
+    disabled ? "rgb(var(--secondary-rgb))" : "transparent"};
+  color: ${({ disabled }) =>
+    disabled ? "rgb(var(--active-secondary-rgb))" : "rgb(var(--primary-rgb))"};
 
   &:hover {
-    cursor: pointer;
-    border: 3px solid rgb(var(--active-primary-rgb));
-    color: rgb(var(--active-primary-rgb));
+    cursor: ${({ disabled }) => (disabled ? "default" : "pointer")};
+    border: ${({ disabled }) =>
+      disabled ? "" : "3px solid rgb(var(--active-primary-rgb))"};
+    color: ${({ disabled }) =>
+      disabled ? "" : "rgb(var(--active-primary-rgb))"};
   }
 
   &:active {
-    width: 145px;
-    max-width: 75%;
-    font-size: calc(var(--default-font-size) - 1px);
-    margin-top: 5px;
+    width: ${({ disabled }) => (disabled ? "" : "145px")};
+    max-width: ${({ disabled }) => (disabled ? "" : "75%")};
+    font-size: ${({ disabled }) =>
+      disabled ? "" : "calc(var(--default-font-size) - 1px)"};
+    margin-top: ${({ disabled }) => (disabled ? "" : "5px")};
   }
 `;
